191d5ae2a515aa53221d87ec898725999622f259
buket99 <kurtulus.buket.23@icloud.com>
2023-06-06 08:35:32 +0200

    added function to read svg-file

--- main.go
+++ main.go
@@ -1,38 +1,36 @@
 package main
 
 import (
-	"encoding/xml"
 	"fmt"
-	"os"
+	"io/ioutil"
 )
 
-// 1. Lesen Sie die SVG-Datei 'DeutschlandMitStaedten.svg'
-// 2. Ermitteln Sie die Flächen der einzelnen Bundesländer (bezüglich der in der Datei verwendeten Skala)
-// 3. Am Ende der Datei befinden sich Koordinaten von Städten,
-//    Versuchen Sie herauszufinden (bzw. lassen Sie das Ihren Rechner machen ;-), in welchem Bundesland diese
-//    jeweils liegen.
-
-type SVG struct {
-	XMLName xml.Name `xml:"svg"`
-	Width   string   `xml:"width,attr"`
-	Height  string   `xml:"height,attr"`
-}
+/*
+Aufgabe:
+	- Lese SVG-Datei
+	- Ermittle die Flächen der einzelnen Bundesländer
+	- Bzgl. der Koordinaten von Städten, finde heraus in welchem Bundesland diese liegen
+*/
 
 func main() {
-	file, err := os.Open("DeutschlandMitStaedten.svg")
+	// Read the SVG file
+	data, err := ioutil.ReadFile("DeutschlandMitStaedten.svg")
 	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
-	}
-	defer file.Close()
-
-	var svg SVG
-	err = xml.NewDecoder(file).Decode(&svg)
-	if err != nil {
-		fmt.Println("Error decoding XML:", err)
+		fmt.Println("Error reading file:", err)
 		return
 	}
 
-	fmt.Printf("SVG width: %s, height: %s", svg.Width, svg.Height)
+	// Convert the byte slice to a string
+	svgContent := string(data)
 
+	// Process the SVG content as needed
+	fmt.Println(svgContent)
+}
+
+func flaecheninhalt() float64 {
+	return 0
+}
+
+func findeBundesland() float64 {
+	return 0
 }
