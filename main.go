858787137d7d89298befcbbc1973423aba15950c
Sara Kosanović <sara.kosanovic@qaware.de>
2023-06-13 16:41:07 +0200

    added city extrahation

--- main.go
+++ main.go
@@ -16,23 +16,105 @@ func main() {
 		fmt.Println("Error:", err)
 		return
 	}
+	var bundeslaender []Bundesland
+	var staedte []City
 
 	svgString := string(svgContent)
 
-	paths := extractPaths(svgString)
-
-	for id, path := range paths {
+	bundeslaenderPaths := extractBundesländerPaths(svgString)
+	for id, path := range bundeslaenderPaths {
+		var bundesland = extractBundesland(path, id)
+		bundeslaender = append(bundeslaender, bundesland)
 		area, err := calculatePathArea(path)
 		if err != nil {
 			fmt.Printf("Error calculating area for %s: %s\n", id, err)
 			continue
 		}
-
 		fmt.Printf("Area for %s: %f\n", id, area)
+	}
+
+	staedtePaths := extractStaedtePaths(svgString)
+	for id, path := range staedtePaths {
+		var stadt = extractStadt(path, id)
+		staedte = append(staedte, stadt)
+		// TODO Buket : hier Berechnung einfügen
+		fmt.Printf("Found following City: %s\n", stadt.id)
 	}
 }
 
-func extractPaths(svgString string) map[string]string {
+func extractStaedtePaths(svgString string) map[string]string {
+	regexPattern := `<path[^>]+id\s*=\s*["']([^"']+)["'][^>]*\s+sodipodi:cx\s*=\s*["']([^"']+)["'][^>]*\s+sodipodi:cy\s*=\s*["']([^"']+)["'][^>]*>`
+	regex := regexp.MustCompile(regexPattern)
+
+	matches := regex.FindAllStringSubmatch(svgString, -1)
+	pathMap := make(map[string]string)
+
+	for _, match := range matches {
+		if len(match) >= 4 {
+			path := match[1]
+			id := match[0]
+			pathMap[path] = id
+		}
+	}
+
+	return pathMap
+}
+
+func extractStadt(path string, id string) City {
+	var stadt City
+	regexPattern := `sodipodi:cx\s*=\s*"([^"]+)"\s+sodipodi:cy\s*=\s*"([^"]+)"`
+	regex, err := regexp.Compile(regexPattern)
+	if err != nil {
+		return stadt
+	}
+
+	match := regex.FindStringSubmatch(path)
+	if len(match) < 3 {
+		return stadt
+	}
+
+	stadt.id = id
+	stadt.coordinate.X, err = strconv.ParseFloat(match[1], 64)
+	stadt.coordinate.Y, err = strconv.ParseFloat(match[2], 64)
+
+	return stadt
+}
+
+func extractBundesland(path string, id string) Bundesland {
+	var absoluteCoordinates []Point
+	coordinates := extractCoordinates(path)
+
+	var lastX, lastY float64
+	var prefix string
+
+	for i := 0; i < len(coordinates)-1; i += 2 {
+		x, err := strconv.ParseFloat(coordinates[i], 64)
+		if err != nil {
+			//return Bundesland{0,[]}, err
+		}
+
+		y, err := strconv.ParseFloat(coordinates[i+1], 64)
+		if err != nil {
+			//return Bundesland{0,[]}, err
+		}
+
+		if i == 0 {
+			prefix = getPrefix(coordinates[0])
+		}
+
+		if i >= 2 && (strings.HasPrefix(coordinates[i], "-") || strings.HasPrefix(coordinates[i+1], "-")) {
+			absX, absY := convertToAbsolute(lastX, lastY, x, y, prefix)
+			absoluteCoordinates = append(absoluteCoordinates, Point{absX, absY})
+		}
+
+		lastX, lastY = x, y
+	}
+
+	return Bundesland{id, absoluteCoordinates}
+
+}
+
+func extractBundesländerPaths(svgString string) map[string]string {
 	regexPattern := `<path[^>]+id\s*=\s*["']([^"']+)["'][^>]*\sd\s*=\s*["']([^"']+)["'][^>]*>`
 	regex, err := regexp.Compile(regexPattern)
 	if err != nil {
@@ -149,4 +231,24 @@ func convertToAbsolute(lastX, lastY, x, y float64, prefix string) (float64, floa
 func calculatePolygonArea(x1, y1, x2, y2, x3, y3 float64) float64 {
 	area := 0.5 * ((x1*y2 + x2*y3 + x3*y1) - (x2*y1 + x3*y2 + x1*y3))
 	return math.Abs(area)
+}
+
+// pro Bundesland koordinaten zurückgeben
+// die Städte einzelne Punkte auslesen
+type Point struct {
+	X, Y float64
+}
+
+type Bundesland struct {
+	id          string
+	coordinates []Point
+}
+
+type City struct {
+	id         string
+	coordinate Point
+}
+
+func hasArcType(svgPath string) bool {
+	return strings.Contains(svgPath, `sodipodi:type="arc"`)
 }
