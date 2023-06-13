3bea878bb94e3c617e7199666ec28e7dcd609035
Sara Kosanović <sara.kosanovic@qaware.de>
2023-06-13 10:20:17 +0200

    commit current version

--- go.mod
+++ go.mod
@@ -1,3 +1,12 @@
 module CompGeo2
 
 go 1.20
+
+require (
+	github.com/ajstarks/svgo v0.0.0-20211024235047-1546f124cd8b // indirect
+	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/tdewolff/minify v2.3.6+incompatible // indirect
+	github.com/tdewolff/parse v2.3.4+incompatible // indirect
+	golang.org/x/image v0.7.0 // indirect
+)
--- main.go
+++ main.go
@@ -3,34 +3,114 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"math"
+	"regexp"
+	"strings"
+
+	"github.com/tdewolff/parse"
 )
 
-/*
-Aufgabe:
-	- Lese SVG-Datei
-	- Ermittle die Flächen der einzelnen Bundesländer
-	- Bzgl. der Koordinaten von Städten, finde heraus in welchem Bundesland diese liegen
-*/
+type Point struct {
+	X float64
+	Y float64
+}
+
+type Path struct {
+	Points []Point
+}
+
+func parseSVGPath(svgPath string) (*Path, error) {
+	path := &Path{}
+	r := strings.NewReader(svgPath)
+
+	for {
+		t, _, _ := parse.Next(r)
+		if t == parse.ErrorToken {
+			break
+		}
+
+		if t == parse.CommandToken {
+			c, _ := parse.ReadCommand(r)
+
+			switch c {
+			case 'm', 'M':
+				for {
+					p, err := parse.ReadFloats(r, 2)
+					if err != nil {
+						break
+					}
+
+					path.Points = append(path.Points, Point{X: p[0], Y: p[1]})
+				}
+			case 'l', 'L':
+				for {
+					p, err := parse.ReadFloats(r, 2)
+					if err != nil {
+						break
+					}
+
+					path.Points = append(path.Points, Point{X: p[0], Y: p[1]})
+				}
+			case 'z', 'Z':
+				// Ignore close path commands
+			default:
+				return nil, fmt.Errorf("Unsupported SVG path command: %c", c)
+			}
+		}
+	}
+
+	return path, nil
+}
+
+func calculatePathArea(path *Path) float64 {
+	area := 0.0
+	n := len(path.Points)
+
+	if n < 3 {
+		return area
+	}
+
+	for i := 0; i < n-1; i++ {
+		area += (path.Points[i].X * path.Points[i+1].Y) - (path.Points[i+1].X * path.Points[i].Y)
+	}
+
+	area += (path.Points[n-1].X * path.Points[0].Y) - (path.Points[0].X * path.Points[n-1].Y)
+
+	return math.Abs(area) / 2.0
+}
 
 func main() {
-	// Read the SVG file
-	data, err := ioutil.ReadFile("DeutschlandMitStaedten.svg")
+	// Read the SVG data from a file
+	svgData, err := ioutil.ReadFile("path/to/svg/file.svg")
 	if err != nil {
-		fmt.Println("Error reading file:", err)
+		fmt.Println("Failed to read SVG file:", err)
 		return
 	}
 
-	// Convert the byte slice to a string
-	svgContent := string(data)
+	// Convert the SVG data to a string
+	svgString := string(svgData)
 
-	// Process the SVG content as needed
-	fmt.Println(svgContent)
-}
+	// Define the regular expression pattern to match the path elements
+	pathRegex := regexp.MustCompile(`<path\s+[^>]*d="([^"]+)"[^>]*>`)
 
-func flaecheninhalt() float64 {
-	return 0
-}
+	// Find all matches of the pattern in the SVG data
+	matches := pathRegex.FindAllStringSubmatch(svgString, -1)
 
-func findeBundesland() float64 {
-	return 0
+	// Iterate over the matches and extract the path coordinates
+	for _, match := range matches {
+		if len(match) >= 2 {
+			svgPath := match[1]
+
+			// Parse the SVG path to get the coordinates
+			path, err := parseSVGPath(svgPath)
+			if err != nil {
+				fmt.Println("Failed to parse SVG path:", err)
+				continue
+			}
+
+			// Calculate the area surrounded by the path
+			area := calculatePathArea(path)
+			fmt.Println("Path Area:", area)
+		}
+	}
 }
